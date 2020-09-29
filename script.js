0933a9b77ff85cf57eb2466417020ae060194299
Mogalathurthi Tarun siva sai durga <49830189+Tarunssd@users.noreply.github.com>
2020-09-29 21:36:04 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+const daysEL = document.getElementById('days');
+const hoursEL = document.getElementById('hours');
+const minsEL = document.getElementById('mins');
+const secondsEL = document.getElementById('seconds');
+
+const newyear = '1 jan 2021';
+
+function countdown() {
+    const newYearsDate = new Date(newyear);
+    const currentDate = new Date();
+
+    const totseconds = (newYearsDate - currentDate) / 1000;
+    const days = Math.floor(totseconds / 3600 / 24);
+    const hours = Math.floor(totseconds / 3600) % 24;
+    const mins = Math.floor(totseconds / 60) % 60;
+    const seconds = Math.floor(totseconds) %60;
+
+    daysEL.innerHTML = days;
+    hoursEL.innerHTML = formatTime(hours);
+    minsEL.innerHTML = formatTime(mins);
+    secondsEL.innerHTML = formatTime(seconds);
+}
+
+function formatTime(time) {
+    return time < 10 ? `0${time}` : time;
+}
+
+countdown();
+
+setInterval(countdown,1000);
